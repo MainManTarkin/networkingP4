03e40e16dd38b565103127eb241eca2aa20418bd
tarkin <tarkin@tarknet.com>
2023-04-17 15:09:37 -0500

    intial commit

---
+++ networking.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <curses.h>
+#include <thread>
+#include <map>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <memory.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+//functions here
+
