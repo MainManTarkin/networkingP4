d84198d2b940db64b2211a65deff647642bf24ac
tarkin <tarkin@tarknet.com>
2023-04-19 19:52:05 -0500

    improved auto buildshell with better commands and only running if g++ worked

--- buildshell.sh
+++ buildshell.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 OUTPUTNAME="a.out"                           #g++ output name
 VALGRINDLUNCH=0
+RUNAFTER=0
+RETURNVALUE=0
 
+#-o selects output name
+#-r runs progam after compiling it
+#-v means begin runnign in valgrind (only works if run now arg is called)
 
-while getopts "o:v" options; do         # Loop: Get the next option;
+while getopts "o:vr" options; do         # Loop: Get the next option;
                                           # use silent error checking;
                                           # options n and t take arguments.
   case "${options}" in                    # 
+    r)                                    # If the option is v,
+      RUNAFTER=1
+      ;;
     v)                                    # If the option is v,
       VALGRINDLUNCH=1
       ;;
@@ -21,8 +29,21 @@ done
 
 g++ testMain.cpp networking.cpp -o $OUTPUTNAME -Wall -Werror --pedantic -std=c++11 -g 
 
-if [ $VALGRINDLUNCH -eq 1 ]; then
-    valgrind ./$OUTPUTNAME 
+RETURNVALUE=$?
+
+if [ $RETURNVALUE -eq 0 ]; then
+
+    if [ $RUNAFTER -eq 1 ]; then
+
+        if [ $VALGRINDLUNCH -eq 1 ]; then
+            valgrind ./$OUTPUTNAME 
+        else
+            ./$OUTPUTNAME
+        fi
+
+    fi
 else
-    ./$OUTPUTNAME
+
+    printf 'Error with g++: %d\n' $RETURNVALUE
+
 fi
