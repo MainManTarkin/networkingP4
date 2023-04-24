2bd34234be12635134f4e653bef17dc1e63a21e4
tarkin <tarkin@tarknet.com>
2023-04-24 12:13:26 -0500

    build script now passes args to program

--- buildshell.sh
+++ buildshell.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 OUTPUTNAME="a.out"                           #g++ output name
+PASSEDARGS=""           #arguments to pass onto ran program (should be placed in "")
 VALGRINDLUNCH=0
 RUNAFTER=0
 RETURNVALUE=0
+GOTARGS=0
 
 #-o selects output name
 #-r runs progam after compiling it
 #-v means begin runnign in valgrind (only works if run now arg is called)
 
-while getopts "o:vr" options; do         # Loop: Get the next option;
+while getopts "o:a:vr" options; do         # Loop: Get the next option;
                                           # use silent error checking;
                                           # options n and t take arguments.
   case "${options}" in                    # 
@@ -21,29 +23,42 @@ while getopts "o:vr" options; do         # Loop: Get the next option;
     o)
         OUTPUTNAME=${OPTARG}
       ;;
+    a)
+      PASSEDARGS=${OPTARG}
+      ;;
     *)                                    # If unknown (any other) option:
+      GOTARGS=1 #dont run anything after getopt is ran
       exit_abnormal                       # Exit abnormally.
       ;;
   esac
 done
 
-g++ testMain.cpp networking.cpp -o $OUTPUTNAME -Wall -Werror --pedantic -std=c++11 -g 
+#ugly bash if else loop
+if [ $GOTARGS -eq 0 ]; then
 
-RETURNVALUE=$?
+  g++ testMain.cpp networking.cpp -o $OUTPUTNAME -Wall -Werror --pedantic -std=c++11 -g 
 
-if [ $RETURNVALUE -eq 0 ]; then
+  RETURNVALUE=$?
 
-    if [ $RUNAFTER -eq 1 ]; then
+  if [ $RETURNVALUE -eq 0 ]; then
 
-        if [ $VALGRINDLUNCH -eq 1 ]; then
-            valgrind ./$OUTPUTNAME 
-        else
-            ./$OUTPUTNAME
-        fi
+      if [ $RUNAFTER -eq 1 ]; then
 
-    fi
-else
+          if [ $VALGRINDLUNCH -eq 1 ]; then
+              valgrind ./$OUTPUTNAME $PASSEDARGS
+          else
+              ./$OUTPUTNAME $PASSEDARGS
+          fi
 
-    printf 'Error with g++: %d\n' $RETURNVALUE
+        RETURNVALUE=$?
+
+        printf 'Program return code: %d\n' $RETURNVALUE
+
+      fi
+  else
+
+      printf 'Error with g++: %d\n' $RETURNVALUE
+
+  fi
 
 fi
