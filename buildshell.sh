cfdf475bf3e97de83c58c8eaa4667ce8bce2d1a4
tarkin <tarkin@tarknet.com>
2023-04-26 14:17:31 -0500

    added input build file arg -i to buildshell

--- buildshell.sh
+++ buildshell.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 OUTPUTNAME="a.out"                           #g++ output name
 PASSEDARGS=""           #arguments to pass onto ran program (should be placed in "")
+INPUTNAMES="testMain.cpp"
 VALGRINDLUNCH=0
 RUNAFTER=0
 RETURNVALUE=0
@@ -8,12 +9,16 @@ GOTARGS=0
 
 #-o selects output name
 #-r runs progam after compiling it
-#-v means begin runnign in valgrind (only works if run now arg is called)
+#-v means begin running in valgrind (only works if run now arg is called)
+#-i provide input c++ files for g++ defaults to testMain.cpp
 
-while getopts "o:a:vr" options; do         # Loop: Get the next option;
+while getopts "i:o:a:vr" options; do         # Loop: Get the next option;
                                           # use silent error checking;
                                           # options n and t take arguments.
   case "${options}" in                    # 
+    i)
+      INPUTNAMES=${OPTARG}
+      ;;
     r)                                    # If the option is v,
       RUNAFTER=1
       ;;
@@ -36,7 +41,7 @@ done
 #ugly bash if else loop
 if [ $GOTARGS -eq 0 ]; then
 
-  g++ testMain.cpp networking.cpp -o $OUTPUTNAME -Wall -Werror --pedantic -std=c++11 -g 
+  g++ $INPUTNAMES networking.cpp -o $OUTPUTNAME -Wall -Werror --pedantic -std=c++11 -g 
 
   RETURNVALUE=$?
 
